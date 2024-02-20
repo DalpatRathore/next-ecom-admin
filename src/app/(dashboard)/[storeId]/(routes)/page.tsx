3a3af910c400b4e02d565fbb539724dfa99c53f9
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-02-20 13:23:33 +0530

    feat: store switcher

--- src/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,4 +1,18 @@
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+import prismadb from "@/lib/db";
+
+interface DashboardPageProps {
+  params: { storeId: string };
+}
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+  return (
+    <div className="bg-emerald-500">
+      Active Store: <strong>{store?.name}</strong>
+    </div>
+  );
 };
 export default DashboardPage;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import { UserButton, auth } from "@clerk/nextjs";
+import MainNav from "@/components/MainNav";
+import StoreSwitcher from "@/components/StoreSwitcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/db";
+
+const Navbar = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+  return (
+    <div className="border-b">
+      <div className="flex items-center h-16 px-4">
+        <StoreSwitcher items={stores}></StoreSwitcher>
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex-items-center space-x-4">
+          <UserButton afterSignOutUrl="/"></UserButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
