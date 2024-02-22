6d0223b795045903979cd665c196b023641d475a
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-02-22 09:49:06 +0530

    feat: billboard page

--- src/app/(dashboard)/[storeId]/(routes)/billboards/_components/BillboardClient.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/_components/BillboardClient.tsx
@@ -3,17 +3,22 @@
 import Heading from "@/components/Heading";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { Billboard } from "@prisma/client";
 import { PlusCircledIcon } from "@radix-ui/react-icons";
 import { useParams, useRouter } from "next/navigation";
 
-const BillboardClient = () => {
+interface BillboardClientProps {
+  data: Billboard[];
+}
+
+const BillboardClient = ({ data }: BillboardClientProps) => {
   const router = useRouter();
   const params = useParams();
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards (0)"
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         ></Heading>
         <Button
--- src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,19 @@
+import prismadb from "@/lib/db";
 import BillboardClient from "./_components/BillboardClient";
 
-const BillboardsPage = () => {
+const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient></BillboardClient>
+        <BillboardClient data={billboards}></BillboardClient>
       </div>
     </div>
   );
